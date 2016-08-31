a0561d311202acdb3e11bafbba8c4e6633212bfe
MWangxj <wangxianjin028@hotmail.com>
2016-08-31 20:38:35 +0800

    cctv及各卫视直播

---
+++ AiTV/MainTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainTableViewController.h
+//  AiTV
+//
+//  Created by wangxianjin on 16/8/31.
+//  Copyright © 2016年 wangxianjin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainTableViewController : UITableViewController
+
+@end
---
+++ AiTV/TVViewController.h
@@ -0,0 +1,16 @@
+//
+//  TVViewController.h
+//  AiTV
+//
+//  Created by wangxianjin on 16/8/31.
+//  Copyright © 2016年 wangxianjin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TVViewController : UIViewController
+
+@property(strong,nonatomic)NSString* tvTitle;
+@property(strong,nonatomic)NSString*tvURL;
+
+@end
